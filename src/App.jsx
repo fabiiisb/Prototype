1a9055787e3d04c0a6c4da32a6be6e0b2917ac35
Fabiiisb <fabiiisb@hotmail.com>
2023-09-13 10:25:05 -0300

    add: motion animations

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,35 @@ import wire1 from "./assets/wire1.svg";
 import wire2 from "./assets/wire2.svg";
 import wire3 from "./assets/wire3.svg";
 const LazyThreeObject = lazy(() => import("./components/three/mainThree"));
+import { motion } from "framer-motion";
 
 function App() {
   let title = "PROTOTYPE";
 
   return (
     <main>
-      <header>
+      <motion.header
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.35 }}
+      >
         <h1>{title}</h1>
         <h2>{title}</h2>
-      </header>
-      <section>
+      </motion.header>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.4 }}
+      >
         <Suspense fallback={Loading()}>
           <LazyThreeObject />
         </Suspense>
-      </section>
-      <menu>
+      </motion.section>
+      <motion.menu
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.35 }}
+      >
         <div className="menu-slot">
           <img src={wire2} alt="wireframe" />
         </div>
@@ -28,7 +41,7 @@ function App() {
         <div className="menu-slot">
           <img src={wire1} alt="wireframe" />
         </div>
-      </menu>
+      </motion.menu>
     </main>
   );
 }
@@ -37,8 +50,8 @@ export default App;
 
 function Loading() {
   return (
-    <div class="container">
-      <div class="lds-roller">
+    <div className="container">
+      <div className="lds-roller">
         <div></div>
         <div></div>
         <div></div>
